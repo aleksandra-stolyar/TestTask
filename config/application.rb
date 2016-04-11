9a4b23a508ed6d03b2da2656e7f2f1d5bf875663
Aleksandra <aleksandra.stolyar@gmail.com>
2016-04-11 16:56:52 +0300

    bug fixes #7
    serve icons

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ module TestTask
     config.assets.enabled = true
 
     config.assets.paths << Rails.root.join("vendor","assets","bower_components")
-    config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass","assets","fonts")
 
     config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff|woff2)$)
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
